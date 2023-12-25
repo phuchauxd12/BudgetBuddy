4ec0b4c49d385f8bf8bc607fd9efb7c4d299d60c
47Nguyen <ankhanhthinj@gmail.com>
2023-12-25 22:29:16 +0700

    MONGODB API, haven't yet have the correct datatype YET

--- bank_route.py
+++ bank_route.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from bank_models import Bank
 from db import connection
 from bank_schema import bankEntity, banksEntity
+from bson import objectid
 bank = APIRouter()
 
 @bank.get('/')
@@ -14,3 +15,14 @@ async def find_all_bank():
 async def create_bank(bank: Bank):
     connection.local.bank.find(dict(bank))
     return banksEntity(connection.local.bank.find())    
+
+@bank.put('/{id}')
+async def update_bank(id, bank: Bank):
+    connection.local.bank.find_one_and_update({"_id": objectid(id)},{
+        "$set":dict(bank)
+    })
+    return bankEntity(connection.local.bank.find({"_id":objectid(id)}))    
+
+@bank.delete('/{id}')
+async def delete_bank(id, bank: Bank):
+    return bankEntity(connection.local.bank.find_one_and_delete({"_id": objectid(id)}))
