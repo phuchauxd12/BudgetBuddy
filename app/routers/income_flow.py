0ab7322b2926e09b7a587b979f4ab340dba76e65
47Nguyen <ankhanhthinj@gmail.com>
2023-12-29 03:14:26 +0700

    Edit API

--- app/routers/income_flow.py
+++ app/routers/income_flow.py
@@ -8,45 +8,41 @@ from constant import Constant
 bank = APIRouter(prefix="/api/v1")
 connection = MongoClient()
 CLIENT = MongoClient(host=Constant.MONGODB_URI).get_database("dev")
-db = CLIENT.get_collection('IncomeFlow')
-collection = db["IncomeFlowTest"]
+collection = CLIENT.get_collection('IncomeFlow')
+
 
 def income_flow(item) -> dict:
     return  {
-        'id': str(item["_id"]),
-        'date': str('date'),
-        'total_income': str('total_income'),
-        'total_expenses': str('total_expenses')
+        'income_id': str(item["_id"]),
+        'date':str(item['date']),
+        'total_income': item['total_income'],
+        'total_expenses': item['total_expenses']
     }
-    
+
+
 def banksEntity(items)-> list:
     return[income_flow(item) for item in items ]
 
 
 @bank.get('/get_income_data')
-async def find_all_bank():
+async def find_all_income():
     return banksEntity(collection.find())
 
 @bank.post('/input_income_data')
-async def create_bank(bank: Bank):
-    """ 
-    Allow use to manually input there income flow transaction 
-    date: Datetime
-    total_income: Float
-    total_expenses
-    These informaitons are used for user income prediction"""
+async def create_income_flow(bank: Bank):
     collection.insert_one(dict(bank))
-    return banksEntity(collection.find())    
-
-@bank.get('/get_by_id/{id}')
-async def find_all_bank():
-    collection.find_one(dict(bank))
     return banksEntity(collection.find())
 
+
+@bank.get('/get_by_id/{id}')
+async def find_by_id(income_id: str):
+    existing_id = income_flow(collection.find_one({"_id": ObjectId(income_id)}))
+    return existing_id
+
 @bank.put('/update_by_id/{id}')
-async def update_bank(id: str, bank: Bank):
-    collection.find_one_and_update({"_id": ObjectId(id)},{"$set":dict(bank)})
+async def update_income(income_id: str, bank: Bank):
+    return collection.find_one_and_update({"_id": ObjectId(income_id)},{"$set":dict(bank)})
 
 @bank.delete('/delete_by_id/{id}')
-async def delete_bank(id):
-    collection.find_one_and_delete({"_id": ObjectId(id)})
+async def delete_income(income_id: str):
+    return collection.find_one_and_delete({"_id": ObjectId(income_id)})
