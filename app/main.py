eb03fc6bf4e756f710097ec529b2473bef862e5f
47Nguyen <ankhanhthinj@gmail.com>
2023-12-29 00:40:02 +0700

    Changed name of the models and fixed typos in README

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from routers import bank,register, newsletter, bank_account, bank_info, profile, login, goal, transaction, bank_account_import, user_bills, stock,plan_spending
+from routers import income_flow,register, newsletter, bank_account, bank_info, profile, login, goal, transaction, bank_account_import, user_bills, stock,plan_spending
 
 app = FastAPI(
     openapi_url="/api/v1/openapi.json",
@@ -33,6 +33,6 @@ app.include_router(transaction.router)
 app.include_router(bank_account_import.router)
 app.include_router(user_bills.router)
 app.include_router(user_bills.router)
-app.include_router(bank)
+app.include_router(income_flow)
 if __name__ == "__main__":
     uvicorn.run("main:app", workers=1, host="0.0.0.0", port=8080)
